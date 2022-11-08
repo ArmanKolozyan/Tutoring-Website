591b0e42de5c5438450cbe32ae27e45b79004fae
ArmanKolozyan <arman.kolozyan@vub.be>
2022-11-08 22:57:32 +0100

    added main page design

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const posts = [
+    {
+      id: 1,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://img.freepik.com/vrije-photo/gelukkige-jonge-vrouwelijke-student-die-notebooks-van-cursussen-vasthoudt-en-naar-de-camera-glimlacht-staande-in-lentekleding-tegen-een-blauwe-achtergrond_1258-70161.jpg?w=2000",
+    },
+    {
+      id: 2,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://img.freepik.com/free-photo/young-attractive-smiling-student-showing-thumb-up-outdoors-campus-university_8353-6394.jpg?w=2000",
+    },
+    {
+      id: 3,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://cdn.gelestatic.it/deejay/sites/2/2022/04/Giorgio_Petrosyan.jpg",
+    },
+    {
+      id: 4,
+      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
+      img: "https://mmadna.nl/wp-content/uploads/2019/09/Schermafbeelding-2019-09-17-om-10.06.38.png",
+    },
+  ];
 
-export default Home
+  return (
+    <div className="home">
+      <link
+        rel="stylesheet"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+      ></link>
+      <div className="posts">
+        {posts.map((post) => (
+          <div className="post" key={post.id}>
+            <div className="img">
+              <img src={post.img} alt="" />
+            </div>
+            <div className="content">
+              <Link className="link" to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+              <div className="stars">
+                <span class="fa fa-star checked fa-2x"></span>
+                <span class="fa fa-star checked fa-2x"></span>
+                <span class="fa fa-star checked fa-2x"></span>
+                <span class="fa fa-star fa-2x"></span>
+                <span class="fa fa-star fa-2x"></span>
+              </div>
+              <p>{post.desc}</p>
+              <Link className="link" to={`/post/${post.id}`}>
+                <button>Read more</button>
+              </Link>
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Home;
