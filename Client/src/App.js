3105910ed2e421d8ccab63a9c1a21b1153b4bc2b
alexinh0 <alexferna07@gmail.com>
2022-11-09 22:26:48 +0100

    latest version

--- Client/src/App.js
+++ Client/src/App.js
@@ -76,7 +76,7 @@ const router = createBrowserRouter([
 function App() {
   return (
     <div className="app"> 
-      <div className="container1">
+      <div className="container">
       <RouterProvider router={router} />
       </div>
     </div>
