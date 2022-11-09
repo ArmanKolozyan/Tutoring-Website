57bf089b17b8fb232e9deb19df4fd59c99ed9abd
ArmanKolozyan <arman.kolozyan@vub.be>
2022-11-09 01:00:33 +0100

    put dummy posts in a separate file

--- Client/src/pages/Home.jsx
+++ Client/src/pages/Home.jsx
@@ -1,34 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {posts} from "./constants"
 
 const Home = () => {
-  const posts = [
-    {
-      id: 1,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-      img: "https://img.freepik.com/vrije-photo/gelukkige-jonge-vrouwelijke-student-die-notebooks-van-cursussen-vasthoudt-en-naar-de-camera-glimlacht-staande-in-lentekleding-tegen-een-blauwe-achtergrond_1258-70161.jpg?w=2000",
-    },
-    {
-      id: 2,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-      img: "https://img.freepik.com/free-photo/young-attractive-smiling-student-showing-thumb-up-outdoors-campus-university_8353-6394.jpg?w=2000",
-    },
-    {
-      id: 3,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-      img: "https://cdn.gelestatic.it/deejay/sites/2/2022/04/Giorgio_Petrosyan.jpg",
-    },
-    {
-      id: 4,
-      title: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
-      desc: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. A possimus excepturi aliquid nihil cumque ipsam facere aperiam at! Ea dolorem ratione sit debitis deserunt repellendus numquam ab vel perspiciatis corporis!",
-      img: "https://mmadna.nl/wp-content/uploads/2019/09/Schermafbeelding-2019-09-17-om-10.06.38.png",
-    },
-  ];
-
   return (
     <div className="home">
       <link
