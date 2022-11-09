78da3b0dd4d38bfc6083729b75985a2c7c94a734
alexinh0 <alexferna07@gmail.com>
2022-11-09 21:41:33 +0100

    some bugs resolved

--- Client/src/components/Navigationbar.jsx
+++ Client/src/components/Navigationbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import logo from "../images/Logo.png"
 
-import "bootstrap/dist/css/bootstrap.css";
-import "../style.scss"
-import { Nav, Navbar, NavDropdown, Container } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.css"; // HIER IS EEN PROBLEEM
+import {Nav, Navbar, NavDropdown} from "react-bootstrap";
 import * as Icon from 'react-bootstrap-icons';
+import "../style.scss";
 
 const Navigationbar = () => {
   return (
@@ -22,7 +22,7 @@ const Navigationbar = () => {
         <Nav.Link href="#Summeries"> <Icon.JournalBookmarkFill size={20} /> Summeries</Nav.Link>
         <Nav.Link href="#Search"><Icon.Search size={20} /> Search</Nav.Link>
 
-
+        
         <NavDropdown className="ms-auto" title=<Icon.Bell size={25} /> id="collasible-nav-dropdown">
           <NavDropdown.Item href="#action/dummienoti1clicked">Dummie Notification1</NavDropdown.Item>
           <NavDropdown.Item href="#action/dummienoti2clicked">Dummie Notification2</NavDropdown.Item>
@@ -30,7 +30,7 @@ const Navigationbar = () => {
           <NavDropdown.Divider />
           <NavDropdown.Item href="#action/3.4">All Notifications</NavDropdown.Item>
         </NavDropdown>
-        
+
         
         <Nav.Link href="#Profile"> <Icon.PersonBoundingBox size={30} /> My Profile</Nav.Link>
         <Nav.Link href="#Logout"><Icon.BoxArrowRight size={20} /> Logout</Nav.Link>
