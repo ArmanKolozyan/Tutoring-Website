2ab3849d8636c9781d21ee06dbc102445f71ee27
alexinh0 <alexferna07@gmail.com>
2022-12-22 10:13:17 +0100

    New Footer

--- Client/src/components/Footer.jsx
+++ Client/src/components/Footer.jsx
@@ -27,26 +27,26 @@ const Footer = () => {
     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-0">
     <h6 class="text-uppercase">Useful links</h6>
     <hr/>
-    <p><a href="#">Link1</a></p>
-    <p><a href="#">Link2</a></p>
-    <p><a href="#">Link3</a></p>
+    <p><a href="https://www.vub.be/en" target="_blank">VUB - ASA Tutoring</a></p>
+    <p><a href="https://www.wikihow.com/Improve-Your-Study-Skills" target="_blank">Improve your study skills</a></p>
+    <p><a href="https://www.wikihow.com/Be-a-Good-Tutor" target="_blank">Become a great tutor</a></p>
     </div>
 
     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-0">
-    <h6 class="text-uppercase">Other things</h6>
+    <h6 class="text-uppercase">Follow us</h6>
     <hr/>
-    <p><a href="#">Link1</a></p>
-    <p><a href="#">Link2</a></p>
-    <p><a href="#">Link3</a></p>
+    <p><a href="#"><Icon.Instagram size={25}/> Instagram</a></p>
+    <p><a href="#"><Icon.Twitter size={25}/> Twitter</a></p>
+    <p><a href="#"><Icon.Linkedin size={25}/> LinkedIn</a></p>
     </div>
 
     <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-0">
     <h6 class="text-uppercase">Contact us</h6>
     <hr/>
-    <p><a href="#"><Icon.House size={25}/> straat,huis#,stad,Belgie</a></p>
-    <p><a href="#"><Icon.Mailbox size={25}/> ...@_ASATUTORINGDOMEIN_.be</a></p>
+    <p><a href="#"><Icon.House size={25}/> Straatlaan 34, 3004 Stad, België</a></p>
+    <p><a href="#"><Icon.Mailbox size={25}/> asatutoring@vub.be</a></p>
     <p><a href="#"><Icon.Telephone size={25}/> +32 123 456 78</a></p>   
-    <p><a href="#"><Icon.Printer size={25}/>+32 123 456 78</a></p>
+    <p><a href="#"><Icon.Printer size={25}/> +32 123 456 78</a></p>
     </div>
   </div>
   </div>
